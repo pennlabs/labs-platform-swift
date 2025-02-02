255770b959516d5cf6895a0a874d90b38c9ee9b6
Jonathan Melitski <jonathanmelitski@gmail.com>
2025-02-02 14:08:53 -0500

    Add preliminary documentation to public functions

--- Sources/Auth/LabsPlatformAuth.swift
+++ Sources/Auth/LabsPlatformAuth.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 extension LabsPlatform {
+    
+    /// Handles the authentication flow with Platform.
+    /// Defined in the scope of the `LabsPlatform` environment object created by [`View.enableLabsPlatform()`](x-source-tag://enableLabsPlatform)
+    ///
+    /// - Tag: loginWithPlatform
     public func loginWithPlatform() {
         beginLogin()
     }
@@ -30,12 +35,39 @@ extension LabsPlatform {
     }
     
     func fetchToken(authCode: AuthCompletionResult) {
-        if case .newLogin(let currentState, _) = authState, currentState != authCode.state  {
+        guard case .newLogin(let currentState, let verifier) = authState, currentState == authCode.state else {
             authState = .loggedOut
             print("OAuth state did not match! This could be a bug, or a sign of a highly sophisticated CSRF attack.")
             exit(1)
         }
         
+        let parameters: [String: String] = [
+            "grant_type": "authorization_code",
+            "code": authCode.authCode,
+            "redirect_uri": self.redirectUrl.absoluteString,
+            "client_id": self.clientId,
+            "code_verifier": verifier,
+            "state": authCode.state
+        ]
+        let parameterArray = parameters.map { "\($0.key)=\($0.value)" }
+        let postString = parameterArray.joined(separator: "&")
+        
+        let postData =  postString.data(using: .utf8)
+
+        var request = URLRequest(url: LabsPlatform.tokenEndpoint)
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = postData
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
         //form post request
         
         //also cache
--- Sources/LabsPlatform.swift
+++ Sources/LabsPlatform.swift
@@ -79,6 +79,17 @@ struct PlatformProvider<Content: View>: View {
 }
 
 public extension View {
+    /// Enables Labs Platform for all subviews.
+    ///
+    /// > Note: This should be called on the highest SwiftUI View in the view hierarchy, regardless of whether there is a guest mode or not.
+    /// > That is, call this method at the highest view in the viewport, then at the time of login, call [`LabsPlatform.loginWithPlatform()`](x-source-tag://loginWithPlatform)
+    ///
+    /// - Parameters:
+    ///     - clientId: A Platform-granted clientId that has permission to get JWTs
+    ///     - redirectUrl: A valid redirect URI (allowed by the Platform application)
+    ///
+    /// - Returns: The original view with a `LabsPlatform` environment object.
+    /// - Tag: enableLabsPlatform
     func enableLabsPlatform(clientId: String, redirectUrl: URL) -> some View {
         return PlatformProvider(clientId: clientId, redirectUrl: redirectUrl) {
             self
